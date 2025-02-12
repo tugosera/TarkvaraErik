ad00347b21bec8216e4864ba9ccadb63ae016dfd
Erik Gerega <154507933+tugosera@users.noreply.github.com>
2025-02-12 11:11:28 +0200

    Delete kalkScript.js

--- kalkScript.js
+++
@@ -1,8 +0,0 @@
-function plus()
-{
-    let num1=document.getElementById("num1");
-    let num2=document.getElementById("num2");
-
-    let l = Number(num1.value) + Number(num2.value);
-    vastus4.innerHTML="vastus on " + l;
-}
