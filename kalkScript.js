005a73af230b0f5b6770a911e26b760c30646d8f
Anton Ivanov <anton9032@gmail.com>
2025-02-16 21:10:38 +0200

    Rakendatud funktsioonid: liitmine, lahutamine, korrutamine ja jagamine (kontrollides nulliga jagamist).

---
+++ kalkScript.js
@@ -0,0 +1,26 @@
+function calculate(operation) {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let result;
+
+    if (isNaN(num1) || isNaN(num2)) {
+        result = "Palun sisesta numbrid!";
+    } else {
+        switch (operation) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num2 !== 0 ? num1 / num2 : "Viga: nulliga jagamine!";
+                break;
+        }
+    }
+
+    document.getElementById("vastus4").innerText = "Vastus: " + result;
+}
