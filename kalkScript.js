0435f2d4aedd75644a8dedfee4410b4f92756caa
Anton Ivanov <anton9032@gmail.com>
2025-02-16 21:20:49 +0200

    lisatud veel 2 nuppu, mille abil saab astmes arvu püstitada ja juur numbrist välja võtta

--- kalkScript.js
+++ kalkScript.js
@@ -2,13 +2,13 @@ function calculate(operation) {
     let num1 = parseFloat(document.getElementById("num1").value);
     let num2 = parseFloat(document.getElementById("num2").value);
     let resultElement = document.getElementById("vastus4");
+    let result;
 
-    if (isNaN(num1) || isNaN(num2)) {
+    if (isNaN(num1) || (isNaN(num2) && operation !== "√")) {
         resultElement.innerText = "Palun sisesta kehtivad numbrid!";
         return;
     }
 
-    let result;
     switch (operation) {
         case "+":
             result = num1 + num2;
@@ -22,8 +22,13 @@ function calculate(operation) {
         case "/":
             result = num2 !== 0 ? num1 / num2 : "Viga: nulliga jagamine!";
             break;
+        case "^":
+            result = Math.pow(num1, num2);
+            break;
+        case "√":
+            result = num1 >= 0 ? Math.sqrt(num1) : "Viga: negatiivsest arvust ei saa juurt võtta!";
+            break;
     }
-
     resultElement.innerText = "Vastus: " + result;
 }
 
